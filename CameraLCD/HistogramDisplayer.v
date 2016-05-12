9b8d3983126e3c8b80c62160bcaf630a2ac0de7c
James Anastasiou <james.anastasiou@gmail.com>
2016-05-12 20:29:28 +1000

    Revert "junk" Fixed my mistake This reverts commit 227ff60624d18ea9896d59b90520bdd545984be9.

--- CameraLCD/HistogramDisplayer.v
+++ CameraLCD/HistogramDisplayer.v
@@ -2,20 +2,6 @@ module HistogramDisplayer(
 	input iClk,
 	input iRst_n,
 	input [15:0] X_Cont,
-	input [15:0] Y_Cont,
-	input [19:0] iHistoValue,
-	output [7:0] oHistoAddr,
-	output [7:0] oPixel);
-
-/* Pull in the histogram value based on Y_Cont from the RAM */
-assign oHistoAddr = Y_Cont;
-
-always @(posedge iClk) begin
-	if (X_Cont < iHistoValue) begin
-		oPixel <= -1;
-	end else begin
-		oPixel <= 0;
-	end
-end
-
-endmodule
+	input [15:0] Y_Cont, 
+	output 
+						)
